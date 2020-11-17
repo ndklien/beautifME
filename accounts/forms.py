aafc8350666667ad21aacb8264a1f9ab74a0091f
Lien Nguyen <ndklien3004@gmail.com>
2020-11-17 09:38:32 +0700

    Update registration forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -29,4 +29,4 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2',
-                  'fullName', 'birth']
+                  'fullName', 'birth', 'gender', 'skintype', 'skincondition']
