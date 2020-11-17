dcc9ae3e75ff779f7bc0ed17f2049ee1f83a5f78
Lien Nguyen <ndklien3004@gmail.com>
2020-11-17 15:31:24 +0700

    Make changes on models Product and User

--- product/models.py
+++ product/models.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 import datetime
 from django.contrib.auth.models import User
 
+#pip install Pillow for image import
+
 # Create your models here.
 
 
@@ -13,6 +15,12 @@ class Product(models.Model):
     # Giai thich ve spham
     description = models.TextField()
 
+    #dung tich san pham
+    product_size_in_ml = models.IntegerField(default=0, null=False) 
+
+    #hinh minh hoa spham
+    product_img = models.ImageField(upload_to='product/images/')
+
     # Gia spham
     price = models.IntegerField(default=0)
 
@@ -21,6 +29,8 @@ class Product(models.Model):
         ('DRY', 'Dry Skin'), 
         ('OIL', 'Oliy Skin'), 
         ('COMBI', 'Combination Skin'),
+        ('NORM', 'Normal Skin'),
+        ('ALL', 'All Skin Type'),
     ]
 
     skintype = models.CharField(max_length=5, choices=SKINTYPE_CHOICE)
@@ -32,7 +42,7 @@ class Product(models.Model):
         ('AGE', 'Aging Skin'),
     ]
 
-    skin_cond = models.CharField(max_length=5, choices=SKINCOND_CHOICE)
+    skin_cond = models.CharField(max_length=5, choices=SKINCOND_CHOICE, null=True)
 
     # Loai san pham: SRM, Toner, v.v.
     CATEGORY_CHOICE = [
