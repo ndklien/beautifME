2ee2b43e8a2a592ba2927b7caf626c85778d946d
ndklien <18520298@gm.uit.edu.vn>
2020-11-19 10:18:09 +0700

    Change pub_date to DateField

--- news/models.py
+++ news/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class News(models.Model):
 
     title = models.CharField(max_length=100, blank=True, null=True)
-    pub_date = models.DateTimeField(timezone.datetime.now())
+    pub_date = models.DateField(auto_now_add=True)
     summary = models.TextField(max_length=255)
     content = models.TextField()
 
