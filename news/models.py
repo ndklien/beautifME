556ab9bce2deeb01f870b826d46c601fa91b2032
ndklien <18520298@gm.uit.edu.vn>
2020-11-26 08:05:32 +0700

    change models news and products

--- news/models.py
+++ news/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 class News(models.Model):
 
     title = models.CharField(max_length=100, blank=True, null=True)
+    news_img = models.ImageField(upload_to='news/images/')
     pub_date = models.DateField(auto_now_add=True)
     summary = models.TextField(max_length=255)
     content = models.TextField()
