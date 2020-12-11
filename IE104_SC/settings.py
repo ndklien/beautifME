513a407916ffafafeb723162a306cc7acce03727
Lien Nguyen <ndklien3004@gmail.com>
2020-12-11 17:23:06 +0700

    fix heroku bug

--- IE104_SC/settings.py
+++ IE104_SC/settings.py
@@ -137,6 +137,3 @@ LOGIN_REDIRECT_URL = 'register'
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
-
-import django_heroku
-django_heroku.settings(locals())
