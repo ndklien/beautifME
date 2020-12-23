6301b71e5698d7658d4ea8b48b764fe0e861863e
HoBocPhat <18520196@gm.uit.edu.vn>
2020-12-23 18:05:16 +0700

    add change password features

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render, redirect
 from django.views import generic
 
-from django.contrib.auth import logout
-
-#For registration
-from django.contrib.auth.forms import PasswordChangeForm
-from .forms import RegisterForm,LoginForm, EditProfileForm
-from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.contrib.auth import update_session_auth_hash
 from django.urls import reverse
+
+#For registration
+from django.contrib.auth.models import User
+
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import PasswordChangeForm
+from .forms import RegisterForm,LoginForm, EditProfileForm
+from django.contrib.auth import authenticate, login
+
+from django.contrib.auth import logout
+from django import forms
+
 
 #For login and logout
 
@@ -38,3 +44,49 @@ class LogoutView(generic.View):
     def get(self, request):
         response = logout(request)
         return render(response, self.template_name)
+
+class ProfileView(generic.View):
+
+    template_name = 'accounts/profile.html'
+
+    def get(self, request):
+        return render(response, self.template_name)
+
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(data=request.POST, user=request.user)
+
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return render(request, 'accounts/changepass_success.html')
+        else:
+            return render(request, 'accounts/change_password.html', {'form': form})
+    else:
+        form = PasswordChangeForm(user=request.user)
+
+        args = {'form': form}
+        return render(request, 'accounts/change_password.html', args)
+
+def login_user(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                username = cd['username'],
+                                password = cd['password'])
+
+            if user is not None:
+                login(request, user)
+                return render(request, 'product/base_home.html',{'user': user})
+            else:
+                return HttpResponse('Fail! Please try again!')
+
+    else: 
+        form = LoginForm()
+
+    return render(request, 'accounts/login.html',{'form': form})
+ 
